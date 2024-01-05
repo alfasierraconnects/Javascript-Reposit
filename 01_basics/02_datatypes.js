b31b5cc4384fa051e58aede4e5c06593ed174679
alfasierraconnects <139907988+alfasierraconnects@users.noreply.github.com>
2024-01-05 22:46:45 +0530

    Add files via upload

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treats all JS Code as new version
+//tc:39.es  official ecma script inventory
+
+//primitive Datatypes
+let name = "Ashutosh";
+let age = 25;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigInt => used in large scale application like facebook/Insta
+//string =>""
+//boolean => treue/false
+//null => standalone value
+//undefined
+let state; //here value is undefined
+//symbol
+
+const firstPerson = {};
+const Array = []; // object
+let temperature = null; // object
+
+console.log(typeof Array, typeof firstPerson);
+console.log(typeof null); // null is Object
