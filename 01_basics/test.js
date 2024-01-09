dd1f58802c6f1d04dd31116c9b5c44bc7f062cf2
alfasierraconnects <139907988+alfasierraconnects@users.noreply.github.com>
2024-01-09 22:59:09 +0530

    Add files via upload

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,29 @@
-console.log('Ashutosh');
+console.log("Ashutosh");
+
+let n = 5;
+
+if (n <= 0) {
+  console.log("Enter a positive No.");
+} else if (n === 1) {
+  console.log(2);
+} else {
+  let printedNum = 1;
+  let j = 3;
+  console.log(2);
+  while (printedNum < n) {
+    if (isPrime(j) === true) {
+      console.log(j);
+      printedNum++;
+    }
+    j++;
+  }
+}
+
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
